baa5f2f7b12360317d565da0a9d093c3044e522b
HezenginCom <zengineren2003@gmail.com>
2023-01-29 15:00:32 +0100

    week1 opdracht House

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,6 +5,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.geom.Line2D;
+
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -17,6 +19,27 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
+        graphics.draw(new Line2D.Double(250, 0, 400, 200));
+        graphics.draw(new Line2D.Double(250,0,100,200));
+        graphics.draw(new Line2D.Double(400,200,100,200));
+        graphics.draw(new Line2D.Double(100,200,100,400));
+        graphics.draw(new Line2D.Double(400,200,400,400));
+        graphics.draw(new Line2D.Double(100,400,400,400));
+
+        graphics.draw(new Line2D.Double(150,250,150,400));
+        graphics.draw(new Line2D.Double(200,250,200,400));
+        graphics.draw(new Line2D.Double(150,250,200,250));
+
+        graphics.draw(new Line2D.Double(250,250,250,350));
+        graphics.draw(new Line2D.Double(350,250,350,350));
+
+        graphics.draw(new Line2D.Double(250,250,350,250));
+        graphics.draw(new Line2D.Double(250,350,350,350));
+
+
+
+
+
     }
 
 
