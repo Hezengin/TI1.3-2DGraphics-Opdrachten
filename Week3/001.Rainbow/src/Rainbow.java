d8b1ab3d02ad3eed1cfa4ad9d0eefcc3faf4e724
Hezengin <he.zengin@student.avans.nl>
2023-02-26 23:38:34 +0100

    week 3 regenboog

--- Week3/001.Rainbow/src/Rainbow.java
+++ Week3/001.Rainbow/src/Rainbow.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javafx.application.Application;
 
@@ -14,7 +16,7 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Rainbow extends Application {
     private ResizableCanvas canvas;
-
+    private ArrayList<String> array = new ArrayList<>();
     @Override
     public void start(Stage stage) throws Exception
     {
@@ -27,12 +29,35 @@ public class Rainbow extends Application {
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
     }
 
-
     public void draw(FXGraphics2D graphics)
     {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        graphics.translate(this.canvas.getWidth() / 2, this.canvas.getHeight() / 2);
+        graphics.scale(1, 1);
+
+        Collections.addAll(array,"R","e","g","e","n","b","o","o","g");
+        Color[] colors = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN, Color.BLUE, new Color(128, 0, 128)};
+
+        Font font = new Font(Font.MONOSPACED,Font.BOLD,40);
+        for (String s : array) {
+            Shape shape = font.createGlyphVector(graphics.getFontRenderContext(),s).getOutline();
+            graphics.draw(shape);
+            shape.
+            update();
+        }
+
+
+    }
+
+    public void update(FXGraphics2D graphics){
+        int a = 50;
+        AffineTransform transform = new AffineTransform();
+        transform.rotate(Math.toRadians(20));
+        transform.translate(,a);
+        graphics.setTransform(transform);
     }
 
 
