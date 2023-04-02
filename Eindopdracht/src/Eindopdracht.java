edaa8678ebef2b768e753a3e15a430b3b7f79045
Hezengin <he.zengin@student.avans.nl>
2023-04-03 01:06:02 +0200

    comments

--- Eindopdracht/src/Eindopdracht.java
+++ Eindopdracht/src/Eindopdracht.java
@@ -2,29 +2,26 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.ComboBox;
-import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
-import org.jfree.fx.ResizableCanvas;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.SplittableRandom;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Eindopdracht extends Application {
 
     static Canvas canvas;
     private FXGraphics2D g2d;
-    private ArrayList<Planet> planets = new ArrayList<>();
+    private List<Planet> planets = new CopyOnWriteArrayList<>();
+    private ArrayList<Planet> toBeRemoved = new ArrayList<>(); // tijdelijk
 
     //Images
     private BufferedImage background;
@@ -70,6 +67,9 @@ public class Eindopdracht extends Application {
         primaryStage.setTitle("Planet Simulation");
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.show();
+
+//        canvas.setOnMousePressed(event -> createRandomPlanet("AAA", new Point2D.Double((event.getX() - canvas.getWidth() / 2) * Planet.AU / 32,
+//                (event.getY() - canvas.getHeight() / 2) * Planet.AU / 32)));
     }
 
     public void init() {
@@ -90,12 +90,13 @@ public class Eindopdracht extends Application {
 
             System.out.println("Images Loaded");
 
+            //Planet creations
             Planet sun = new Planet("sun", sunImage, 0, 0, 20, Color.YELLOW, 1.98892 * Math.pow(10, 30));
-            sun.setYVel(10);
+            sun.setYVel(0);
 
 //            funny mode
-//            Planet sun2 = new Planet("sun", sunImage, 30.1 * Planet.AU, 15 * Planet.AU, 20, Color.YELLOW, 1.98892 * Math.pow(10, 30));
-//            sun.setYVel(10);
+//            Planet sun2 = new Planet("sun", sunImage, 10 * Planet.AU, 0 * Planet.AU, 20, Color.YELLOW, 1.98892 * Math.pow(10, 30));
+//            sun2.setYVel(0);
 
             Planet mercury = new Planet("mercury", mercuryImage, 0.387 * Planet.AU, 0, 2, Color.DARK_GRAY, 3.3 * Math.pow(10, 23));
             mercury.setYVel(-47.4 * 1000);
@@ -121,6 +122,11 @@ public class Eindopdracht extends Application {
             Planet neptune = new Planet("neptune", neptuneImage, 30.1 * Planet.AU, 0, 9, Color.BLUE, 1.0243 * Math.pow(10, 26));
             neptune.setYVel(-5.46 * 1000);
 
+            //funny more random planets creations
+//            for (int i = 0; i < 25; i++) {
+//                createRandomPlanet(i + "", new Point2D.Double(Math.random() * 15 * Planet.AU * ((Math.random() > .5) ? 1 : -1), 0));
+//            }
+
             Collections.addAll(planets, sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune);
 
             for (Planet planet : planets) {
@@ -130,6 +136,22 @@ public class Eindopdracht extends Application {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * creates random Earth planets around the canvas on random points with random masses
+     * @param name
+     * @param pos
+     */
+    private void createRandomPlanet(String name ,Point2D pos) {
+        Planet planet = new Planet( name, earthImage,
+                pos.getX(),
+                pos.getY(),
+                (int) (Math.random() * 10),
+                Color.getHSBColor((float) Math.random(), 1, 1),
+                Math.random() * 10 * Math.pow(10, Math.random() * 30));
+        planet.setYVel(Math.random() * 2500 + 5000);
+        planets.add(planet);
     }
 
     private void draw(FXGraphics2D graphics) {
@@ -144,7 +166,57 @@ public class Eindopdracht extends Application {
 
     public void update(double deltaTime) {
         for (Planet planet : planets) {
+            if (checkForCollision(planet)) {
+                planets.remove(planet);
+            }
             planet.update(planets);
         }
+        planets.removeAll(toBeRemoved);
+    }
+
+    /**
+     * Checks for collision when 2 planets crash they become 1
+     * @param planet
+     * @return
+     */
+    public boolean checkForCollision(Planet planet) {
+
+        for (Planet otherPlanet: planets ) {
+            if (planet == otherPlanet || toBeRemoved.contains(otherPlanet) || toBeRemoved.contains(planet))
+                continue;
+
+            Point2D pos = planet.getPosition();
+            Point2D otherPos = otherPlanet.getPosition();
+
+            Point2D lineBetweenPlanets = new Point2D.Double(pos.getX() - otherPos.getX(), pos.getY() - otherPos.getY());
+            double distance = Math.sqrt(lineBetweenPlanets.getX() * lineBetweenPlanets.getX() + lineBetweenPlanets.getY() * lineBetweenPlanets.getY());
+            double minDistance = (planet.getRadius() + otherPlanet.getRadius()) / 10;
+            if (distance < minDistance) {
+                System.out.println("COLLISION");
+
+                //We get the average of both positions and we scale them to
+                //First it subtracts half the canvas width and height from each position to set the 0,0 point at the center of the canvas
+                //then we divide it by SCALE that fits the Canvas
+                double x = (((pos.getX() - canvas.getWidth() / 2) / Planet.SCALE) + ((otherPos.getX() - canvas.getWidth() / 2) / Planet.SCALE))/2;
+                double y = (((pos.getY() - canvas.getHeight() / 2) / Planet.SCALE) + ((otherPos.getY() - canvas.getHeight() / 2) / Planet.SCALE))/2;
+
+                //Creating the combined planet by adding its radius and mass ont the position it crashed
+                Planet combinedPlanet = new Planet("COMBINEDPLANET",
+                        sunImage,
+                        x,
+                        y,
+                        planet.getRadius() + otherPlanet.getRadius(),
+                        Color.WHITE,
+                        planet.getMass() + otherPlanet.getMass());
+                //setting the velocity of combined planets slower because if they crash to each other its normal
+                combinedPlanet.setXVel((planet.getVelocity().getX() / planet.getMass() + otherPlanet.getVelocity().getX() / otherPlanet.getMass()) * 1000);
+                combinedPlanet.setYVel((planet.getVelocity().getY() / planet.getMass() + otherPlanet.getVelocity().getY() / otherPlanet.getMass()) * 1000);
+                toBeRemoved.add(planet);
+                toBeRemoved.add(otherPlanet);
+                planets.add(combinedPlanet);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Eindopdracht/src/Planet.java
+++ Eindopdracht/src/Planet.java
@@ -1,10 +1,8 @@
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.transform.Affine;
 import org.jfree.fx.FXGraphics2D;
 
 import java.awt.*;
 
-import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +10,9 @@ import java.util.List;
 
 public class Planet {
     static double AU = 149.6e6 * 1000;//austronomical unit in km 28.8
-    private static double SCALE = 30 / AU;//1 AU = 100 pixels
+    static double SCALE = 30 / AU;//1 AU = 100 pixels
     private double G = 6.67428e-11;//gravity constant
-    private double TIMESTEP = 3600 * 24;//1 day
+    private double TIMESTEP = 3600 * 24;//was supposed to be 1 day
     private int radius;
     private double x;
     private double y;
@@ -22,21 +20,16 @@ public class Planet {
     private double xVel;
     private double yVel;
     private Color color;
-    private boolean isSun;
     private String name;
     private List<Point> orbit;
     private final double sunMass = 1.989E30;
-
+    private double newX;
+    private double newY;
     private double orbitalPeriodInSeconds;
     private double time = 0;
-
     private BufferedImage image;
     private int[] xPoints;
     private int[] yPoints;
-
-    public Planet() {
-
-    }
 
     public Planet(String name, BufferedImage image, double x, double y, int radius, Color color, double mass) {
         this.name = name;
@@ -51,20 +44,41 @@ public class Planet {
         this.xVel = 0;
         this.orbit = new ArrayList<>();
 
+        //formula to calculate the orbital period
         this.orbitalPeriodInSeconds = 2 * Math.PI * Math.sqrt(Math.pow(Math.abs(x), 3) / (G * sunMass));
     }
 
     public void draw(FXGraphics2D graphics) {
-        double x = this.x * SCALE + Eindopdracht.canvas.getWidth() / 2;
-        double y = this.y * SCALE + Eindopdracht.canvas.getHeight() / 2;
+        graphics.setColor(this.color);
 
-        this.orbit.add(new Point((int) x, (int) y));
         if (this.orbit.size() > 2) {
+            graphics.drawPolyline(xPoints, yPoints, orbit.size());
+        }
 
-            graphics.setColor(this.color);
+        if (this.name.equals("saturn")) {
+            graphics.drawImage(this.image, (int) (newX - radius), (int) (newY - radius / 2), (radius * 2), radius, null);
+        } else {
+            graphics.drawImage(this.image, (int) (newX - radius / 2), (int) (newY - radius / 2), radius, radius, null);
+        }
+    }
+
+    public void update(List<Planet> planetList) {
+        calculateAttraction(planetList);
+        calculateOrbit();
+    }
+
+    private void calculateOrbit() {
+        try {
+            //Scaling the x and y position of the planet on the canvas
+            newX = this.x * SCALE + Eindopdracht.canvas.getWidth() / 2;
+            newY = this.y * SCALE + Eindopdracht.canvas.getHeight() / 2;
+            this.orbit.add(new Point((int) newX, (int) newY));//begin point
+
+            //times 1.2 so that we avoid parts where we dont draw the lines
             if (this.time > this.orbitalPeriodInSeconds * 1.2) {
                 this.orbit.remove(0);
             }
+
             List<Point> updatedList = new ArrayList<>();
             for (Point point : this.orbit) {
                 int px = point.x;
@@ -72,26 +86,28 @@ public class Planet {
 
                 updatedList.add(new Point(px, py));
             }
-
+            // 2 arrays so we can use drawPolyLine method
             xPoints = new int[updatedList.size()];
             yPoints = new int[updatedList.size()];
 
+            //setting each element
             for (int i = 0; i < updatedList.size(); i++) {
                 Point p = updatedList.get(i);
                 xPoints[i] = p.x;
                 yPoints[i] = p.y;
             }
-            graphics.drawPolyline(xPoints, yPoints, updatedList.size());
-        }
+        } catch (Exception e) {
 
-        if (this.name.equals("saturn")) {
-            graphics.drawImage(this.image, (int) (x - radius), (int) (y - radius / 2), (radius * 2), radius, null);
-        } else {
-            graphics.drawImage(this.image, (int) (x - radius / 2), (int) (y - radius / 2), radius, radius, null);
         }
     }
 
-    public void update(ArrayList<Planet> planetList) {
+    /**
+     * calculates the attraction between the current planet object and a list of other planet objects passed as a parameter.
+     * formula: F = G * (m1 * m2)/ r^2 to calculate the force between them
+     * formula : a = F/M to calculate the acceleration of the current planet
+     * @param planetList
+     */
+    private void calculateAttraction(List<Planet> planetList) {
         this.time += TIMESTEP;
 
         double totalForceX = 0;
@@ -100,26 +116,54 @@ public class Planet {
         for (Planet planet : planetList) {
             if (this == planet) continue;
 
-            double distanceX = planet.x - this.x;
-            double distanceY = planet.y - this.y;
-            double distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+            double distanceX = planet.x - this.x;//the x distance for pythagoras
+            double distanceY = planet.y - this.y;//the y distance for pythagoras
+            double distance = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));//the pythagoras
             double force = (G * this.mass * planet.mass) / Math.pow(distance, 2);
 
             double forceX = force * (distanceX / distance);
             double forceY = force * (distanceY / distance);
 
+            //adding the calculated force to totalForce to get the overall force
             totalForceX += forceX;
             totalForceY += forceY;
         }
-
+        //Calculating the acceleration
         double accelX = totalForceX / this.mass;
         double accelY = totalForceY / this.mass;
+
+        //velocity is being updated based on TIMESTEP and the calculated acceleration
+        //and positions
         this.xVel += accelX * TIMESTEP;
         this.yVel += accelY * TIMESTEP;
+
+        //gives the updated x-y coordinate of the planet based on the given interval(TIMESTEP).
         this.x += this.xVel * TIMESTEP;
         this.y += this.yVel * TIMESTEP;
     }
 
+
+    public Point2D getPosition() {
+        newX = this.x * SCALE + Eindopdracht.canvas.getWidth() / 2;
+        newY = this.y * SCALE + Eindopdracht.canvas.getHeight() / 2;
+        return new Point2D.Double(newX, newY);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public Point2D getVelocity() {
+        return new Point2D.Double(xVel, yVel);
+    }
+
+    public void setXVel(double xVel) {
+        this.xVel = xVel;
+    }
     public void setYVel(double yVel) {
         this.yVel = yVel;
     }
